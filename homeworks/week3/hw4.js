f3731e242abe5a5280dec7b3ce0bbe362d839555
Panoopan <c2200446@gmail.com>
2021-04-26 15:55:56 +0800

    week3 homework

---
+++ homeworks/week3/hw4.js
@@ -0,0 +1,25 @@
+const readline = require('readline')
+
+const lines = []
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
+function solve(line) {
+  const str = line[0]
+  let ans = ''
+  for (let i = str.length - 1; i >= 0; i -= 1) { // 字串倒著印
+    ans += str[i]
+  }
+  if (ans === str) {
+    console.log('True')
+  } else {
+    console.log('False')
+  }
+}
+
+rl.on('close', () => solve(lines))
